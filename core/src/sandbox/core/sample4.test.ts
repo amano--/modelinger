ebb0f9f52abae48c195abc86c8aec6741bcbd0cd
amano-- <amapetas@gmail.com>
2021-07-07 22:28:01 +0900

    0707

--- core/src/sandbox/core/sample4.test.ts
+++ core/src/sandbox/core/sample4.test.ts
@@ -15,7 +15,44 @@ const source = program.getSourceFile('src/sandbox/core/Center.ts')
 describe('ast', () => {
   // Create program
 
+  it('template literal', () => {
+    // const template = `hoge=${1} fuga=${2}`
+
+    // console.log('template=', template)
+
+    var say = '手の中にある一羽 > ヤブの中の二羽'
+    var html = htmlEscape`<div> 私はこのように言いたい : ${say}</div>`
+
+    // タグ関数の例
+    function htmlEscape(literals: TemplateStringsArray, ...placeholders: string[]) {
+      let result = ''
+
+      // プレースホルダをリテラルに埋め込む
+      for (let i = 0; i < placeholders.length; i++) {
+        result += literals[i]
+        result += placeholders[i]
+          .replace(/&/g, '&amp;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+      }
+
+      // 最後のリテラルを追加する
+      result += literals[literals.length - 1]
+      return result
+    }
+
+    console.log('html=', html)
+  })
+
   it('simple source', () => {
+    console.log('program.getTypeCatalog() =', program.getTypeCatalog())
+    // console.log('source=', source)
+
+    // console.log('program=', program.getSourceFileByPath())
+    // console.log('source=', )
+
     if (source) {
       console.log('# Exported Type List\n')
       ts.forEachChild(source, function next(node) {
@@ -57,7 +94,13 @@ function printClassDoc(type: ts.Type, ctorSymbol: ts.Symbol): string {
   // Print properties
   type.getProperties().forEach((p) => {
     buf += '\n- ' + serializeSymbol(p)
-    if (p.name === 'refSample') console.log('refSample=', p)
+    if (p.name === 'refSample') {
+      console.log('refSample=', p)
+      // console.log('refSample.exports=', p.exports)
+      // ref https://stackoverflow.com/questions/59838013/how-can-i-use-the-ts-compiler-api-to-find-where-a-variable-was-defined-in-anothe
+      console.log('import = ', p.declarations[0].getText())
+      // console.log('import = ', p.declarations[0].parent.parent.parent.getText())
+    }
   })
 
   return buf + '\n'
